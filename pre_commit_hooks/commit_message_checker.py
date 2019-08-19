c950754ec6c772641957616f1126642ec67c4a9f
Hossein Kazemi <hossein761@yahoo.com>
2019-08-19 16:25:28 +0200

    Added first version of the hook implementation

--- pre_commit_hooks/commit_message_checker.py
+++ pre_commit_hooks/commit_message_checker.py
@@ -1,39 +1,38 @@
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import unicode_literals
-
-import argparse
-import ast
-import platform
+import datetime
+import re
 import sys
-import traceback
+from math import floor
+from subprocess import check_output
 from typing import Optional
-from typing import Sequence
 
 
-def main(argv=None):  # type: (Optional[Sequence[str]]) -> int
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*')
-    args = parser.parse_args(argv)
+def exit_with_error(message: str) -> None:
+    print(message)
+    sys.exit(1)
 
-    retval = 0
-    for filename in args.filenames:
 
-        try:
-            with open(filename, 'rb') as f:
-                ast.parse(f.read(), filename=filename)
-        except SyntaxError:
-            print('{}: failed parsing with {} {}:'.format(
-                filename,
-                platform.python_implementation(),
-                sys.version.partition(' ')[0],
-            ))
-            print('\n{}'.format(
-                '    ' + traceback.format_exc().replace('\n', '\n    '),
-            ))
-            retval = 1
-    return retval
+def run_command(command: str) -> str:
+    return check_output(command.split()).decode('utf-8').strip()
 
+
+def current_git_branch_name() -> str:
+    return run_command('git symbolic-ref --short HEAD')
+
+
+def extract_jira_issue_key(message: str) -> Optional[str]:
+    project_key, issue_number = r'[A-Z]{2,}', r'[0-9]+'
+    match = re.search(f'{project_key}-{issue_number}', message)
+    return match and match.group(0)
+
+
+def main():
+    # Verify that the branch name is a Jira issue key.
+    git_branch_name = current_git_branch_name()
+    jira_issue_key = extract_jira_issue_key(git_branch_name)
+    if not jira_issue_key:
+        exit_with_error(
+            f'Commit aborted! To continue, please rename your branch to a Jira '
+            f'issue key with:\n$ git branch -m {git_branch_name} <KEY-123>')
 
 if __name__ == '__main__':
-    exit(main())
+    main()
